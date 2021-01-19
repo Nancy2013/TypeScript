3f7ec967a54fa04339217ea058a93f8b4c12e44f
Juliette.Wang <nannan.wang@broadlink.com.cn>
2021-01-19 17:45:45 +0800

    helloworld;

--- demo/helloworld.ts
+++ demo/helloworld.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-19 15:43:02
- * @LastEditTime: 2021-01-19 15:53:16
+ * @LastEditTime: 2021-01-19 17:17:05
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \demo\helloworld.ts
@@ -10,4 +10,4 @@ function greet(person: string) {
   return 'Hello, ' + person;
 }
 
-console.log(greet(1));
+console.log(greet('1'));
